67fe650e9506be37c93025b9df9496b7f9cda6cc
DCameronMauch <cam@douglas-cameron.net>
2013-04-08 10:56:51 -0500

    edge case bug fixed

--- underscore.ps.js
+++ underscore.ps.js
@@ -2,7 +2,7 @@ _.mixin(function(){
   var psdb={};
   function publish(topic){
     var message=_.toArray(arguments).slice(1);
-    _.each(psdb[topic],function(callback){
+    _.each(psdb[topic]||[],function(callback){
       callback.apply(this,message);
     });
   }
@@ -13,8 +13,8 @@ _.mixin(function(){
     psdb[topic]=_.without(psdb[topic],callback);
   }
   function multisub(topics,callback,reset,clear){
-    if(typeof(reset)!='boolean') reset=true;
-    if(typeof(clear)!='boolean') clear=false;
+    if(typeof(reset)!=='boolean') reset=true;
+    if(typeof(clear)!=='boolean') clear=false;
     var msdb={};
     _.each(topics,function(topic){
       msdb[topic]=false
