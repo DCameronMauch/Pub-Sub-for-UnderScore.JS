f5a2dc16690f8636b6e9994a3695e93c4ae850b0
DCameronMauch <cam@douglas-cameron.net>
2013-06-10 14:10:29 -0500

    Update underscore.ps.js

--- underscore.ps.js
+++ underscore.ps.js
@@ -12,19 +12,20 @@ _.mixin(function(){
   function unsubscribe(topic,callback){
     psdb[topic]=_.without(psdb[topic],callback);
   }
-  function multisub(topics,callback,reset,clear){
-    if(typeof(reset)!=='boolean') reset=true;
+  function multisub(topics,callback,reset,clear,preset){
+    if(typeof(reset)!=='boolean') reset=false;
     if(typeof(clear)!=='boolean') clear=false;
+    if(typeof(preset)!=='object') preset={};
     var msdb={};
     _.each(topics,function(topic){
-      msdb[topic]=false
+      msdb[topic]=preset[topic]||false;
       subscribe(topic,function(){
         msdb[topic]=_.toArray(arguments);
         if(clear) unsubscribe(topic,arguments.callee);
         if(_.all(msdb,_.identity)){
           callback.apply(this,_.values(msdb));
           if(reset) _.each(topics,function(topic){
-            msdb[topic]=false
+            msdb[topic]=preset[topic]||false;
           });
         }
       });
